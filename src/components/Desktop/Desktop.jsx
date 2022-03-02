7461b71d2a9dec97755e5e2f1dd792c8dcc9e7ff
SaurabhN16 <91530878+saurabhnimkande@users.noreply.github.com>
2022-03-02 22:15:40 +0530

    Added Background with navbar component

---
+++ src/components/Desktop/Desktop.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from "../Navbar/Navbar";
+import "./Desktop.css";
+export const Desktop = () => {
+  return (
+    <div id="wallpaperDiv">
+      <div>
+        <Navbar></Navbar>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
