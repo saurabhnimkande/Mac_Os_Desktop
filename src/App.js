ebf216caf248fc700cba11000f8909d360c03d7b
SaurabhN16 <91530878+saurabhnimkande@users.noreply.github.com>
2022-03-04 17:03:17 +0530

    added tooltip in docIcons with little animation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Desktop } from "./components/Desktop/Desktop";
-
+import "antd/dist/antd.css";
 function App() {
   return (
     <div className="App">
--- src/components/DocItems/DocItem.jsx
+++ src/components/DocItems/DocItem.jsx
@@ -1,54 +1,87 @@
 import "./DocItem.css";
+import { Tooltip } from "antd";
 export const DocItem = () => {
   return (
     <div id="docItems">
       <div>
-        <img src="icons/launchpad.png" alt="launchpad"></img>
+        <Tooltip title="Launchpad" color={"rgba(7, 7, 7, 0.49)"}>
+          <img src="icons/launchpad.png" alt="launchpad"></img>
+        </Tooltip>
       </div>
       <div>
-        <img src="icons/safari.png" alt="safari"></img>
+        <Tooltip title="Safari" color={"rgba(7, 7, 7, 0.49)"}>
+          <img src="icons/safari.png" alt="safari"></img>
+        </Tooltip>
       </div>
       <div>
-        <img src="icons/messages.png" alt="messages"></img>
+        <Tooltip title="Messages" color={"rgba(7, 7, 7, 0.49)"}>
+          <img src="icons/messages.png" alt="messages"></img>
+        </Tooltip>
       </div>
       <div>
-        <img src="icons/mail.png" alt="mail"></img>
+        <Tooltip title="Mail" color={"rgba(7, 7, 7, 0.49)"}>
+          <img src="icons/mail.png" alt="mail"></img>
+        </Tooltip>
       </div>
       <div>
-        <img src="icons/maps.png" alt="maps"></img>
+        <Tooltip title="Maps" color={"rgba(7, 7, 7, 0.49)"}>
+          <img src="icons/maps.png" alt="maps"></img>
+        </Tooltip>
       </div>
       <div>
-        <img src="icons/photos.png" alt="photos"></img>
+        <Tooltip title="Photos" color={"rgba(7, 7, 7, 0.49)"}>
+          <img src="icons/photos.png" alt="photos"></img>
+        </Tooltip>
       </div>
       <div>
-        <img src="icons/facetime.png" alt="facetime"></img>
+        <Tooltip title="Facetime" color={"rgba(7, 7, 7, 0.49)"}>
+          <img src="icons/facetime.png" alt="facetime"></img>
+        </Tooltip>
       </div>
       <div>
-        <img src="icons/calendar.png" alt="calendar"></img>
+        <Tooltip title="Calendar" color={"rgba(7, 7, 7, 0.49)"}>
+          <img src="icons/calendar.png" alt="calendar"></img>
+        </Tooltip>
       </div>
       <div>
-        <img src="icons/contacts.png" alt="contacts"></img>
+        <Tooltip title="Contacts" color={"rgba(7, 7, 7, 0.49)"}>
+          <img src="icons/contacts.png" alt="contacts"></img>
+        </Tooltip>
       </div>
       <div>
-        <img src="icons/reminders.png" alt="reminders"></img>
+        <Tooltip title="Remainders" color={"rgba(7, 7, 7, 0.49)"}>
+          <img src="icons/reminders.png" alt="reminders"></img>
+        </Tooltip>
       </div>
       <div>
-        <img src="icons/notes.png" alt="notes"></img>
+        <Tooltip title="Notes" color={"rgba(7, 7, 7, 0.49)"}>
+          <img src="icons/notes.png" alt="notes"></img>
+        </Tooltip>
       </div>
       <div>
-        <img src="icons/tv.png" alt="tv"></img>
+        <Tooltip title="TV" color={"rgba(7, 7, 7, 0.49)"}>
+          <img src="icons/tv.png" alt="tv"></img>
+        </Tooltip>
       </div>
       <div>
-        <img src="icons/music.png" alt="music"></img>
+        <Tooltip title="Music" color={"rgba(7, 7, 7, 0.49)"}>
+          <img src="icons/music.png" alt="music"></img>
+        </Tooltip>
       </div>
       <div>
-        <img src="icons/calculator.png" alt="calculator"></img>
+        <Tooltip title="Calculator" color={"rgba(7, 7, 7, 0.49)"}>
+          <img src="icons/calculator.png" alt="calculator"></img>
+        </Tooltip>
       </div>
       <div>
-        <img src="icons/podcasts.png" alt="podcasts"></img>
+        <Tooltip title="Poadcasts" color={"rgba(7, 7, 7, 0.49)"}>
+          <img src="icons/podcasts.png" alt="podcasts"></img>
+        </Tooltip>
       </div>
       <div>
-        <img src="icons/systempreferences.png" alt="systempreferences"></img>
+        <Tooltip title="System" color={"rgba(7, 7, 7, 0.49)"}>
+          <img src="icons/systempreferences.png" alt="systempreferences"></img>
+        </Tooltip>
       </div>
     </div>
   );
